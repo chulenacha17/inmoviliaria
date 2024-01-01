01e0d0dd16540083bdf7653ee4eb1d7645e19efb
cleliajuli1997@gmail.com <cleliajuli1997@gmail.com>
2024-01-01 23:13:02 +0100

    hoy01

--- inmoviliaria.js
+++ inmoviliaria.js
@@ -1,1 +1,17 @@
 //comieso de desarrolo codigo jss
+
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    navbar.classList.toggle('active');
+    menu.classList.toggle('fa-times');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    menu.classList.remove('fa-times');
+
+}
+
+ 
